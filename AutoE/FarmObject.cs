c99ceaba5dfcaf3ffd871d0a68ec42c50f7fa9e7
Saturiff <derg.saturiff@gmail.com>
2025-07-07 23:06:00 +0800

    Add AutoE

---
+++ AutoE/FarmObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Timer = System.Windows.Forms.Timer;
+
+using GregsStack.InputSimulatorStandard;
+using GregsStack.InputSimulatorStandard.Native;
+
+namespace AutoE
+{
+    internal class FarmObject
+    {
+        public FarmObject(IntPtr arkHandle) { this.arkHandle = arkHandle; }
+
+        public Timer farmTimer; // 推土
+
+        public int blackSoltCnt = 0;
+
+        public const int blackSlotDelay = 300;
+
+        private IntPtr arkHandle;
+
+        public bool enableBlackSlotCheck = false;
+
+        public VirtualKeyCode targetKeyCode = VirtualKeyCode.VK_E;
+
+        public int interval = 500;
+
+        public void FarmStart()
+        {
+            farmTimer = new Timer { Interval = interval };
+            farmTimer.Tick += new EventHandler(FarmTimerTick);
+            farmTimer.Start();
+        }
+
+        public void FarmPause()
+        {
+            farmTimer.Stop();
+        }
+
+        private InputSimulator sim = new InputSimulator();
+
+        private static void PressKey(InputSimulator sim, VirtualKeyCode key, bool delay = false)
+        {
+            if (delay)
+            {
+                sim.Keyboard.KeyDown(key);
+                sim.Keyboard.Sleep(100);
+                sim.Keyboard.KeyUp(key);
+
+                return;
+            }
+
+            sim.Keyboard.KeyPress(key);
+        }
+
+        private void FarmTimerTick(object sender, EventArgs e)
+        {
+            PressKey(sim, targetKeyCode);
+        }
+
+        [DllImport("user32")]
+        private static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
---
+++ AutoE/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using GregsStack.InputSimulatorStandard.Native;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace AutoE
+{
+    public partial class MainWindow : Window
+    {
+        private IntPtr arkHandle = FindWindow(null, "ARK: Survival Evolved");
+
+        #region Main
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitFarm();
+        }
+        #endregion
+
+        #region 推土
+        private void Click_B_FarmStart(object sender, RoutedEventArgs e) => FarmStart();
+
+        private void Click_B_FarmStop(object sender, RoutedEventArgs e) => FarmPause();
+
+        private FarmObject farm;
+
+        private void InitFarm()
+        {
+            ToggleFarmButton(true);
+            farm = new FarmObject(arkHandle);
+        }
+
+        private void FarmStart()
+        {
+            ToggleFarmButton(false);
+            farm.FarmStart();
+            L_InfoText.Content = "啟用中";
+        }
+
+        private void FarmPause()
+        {
+            ToggleFarmButton(true);
+            farm.FarmPause();
+            ShowMessage("已停止");
+        }
+
+        private void ToggleFarmButton(bool enable)
+        {
+            B_StartFarm.IsEnabled = enable;
+            B_PauseFarm.IsEnabled = !enable;
+        }
+
+        public void ShowMessage(string msg)
+        {
+            L_InfoText.Content = msg;
+        }
+
+        #endregion
+
+        #region DLL
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        #endregion
+
+        private void CB_ToA_Checked(object sender, RoutedEventArgs e)
+        {
+            farm.targetKeyCode = VirtualKeyCode.VK_A;
+        }
+
+        private void CB_ToA_Unchecked(object sender, RoutedEventArgs e)
+        {
+            farm.targetKeyCode = VirtualKeyCode.VK_E;
+        }
+    }
+}
